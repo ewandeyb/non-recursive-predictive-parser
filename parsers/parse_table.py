2a4af2795375476f8deec17365846020bca9b659
Sharmaigne Angelie Mabano <sharmaignemabano@gmail.com>
2025-11-01 22:05:59 +0800

    feat: create parse table from input text

---
+++ parsers/parse_table.py
@@ -0,0 +1,34 @@
+from typing import TextIO
+
+
+def create_parse_table(ptbl: TextIO) -> dict[str, dict[str, int]]:
+    """Creates a parse table from a given file pointer.
+
+    Format of return:
+    {
+        'NonTerminal1': {
+            'Terminal1': rule_index,
+            'Terminal2': rule_index,
+            ...
+        },
+        'NonTerminal2': {...}
+    }
+    """
+
+    parse_table: dict[str, dict[str, int]] = {}
+    # Extract headers
+    headers = (
+        ptbl.readline().strip().split(",")[1:]
+    )  # Skip the first empty cell
+
+    for line in ptbl:  # from second line to end
+        parts = line.strip().split(",")
+        non_terminal = parts[0]
+        parse_table[non_terminal] = {}
+
+        for i, entry in enumerate(parts[1:]):
+            terminal = headers[i]
+            if entry:
+                parse_table[non_terminal][terminal] = int(entry)
+
+    return parse_table
