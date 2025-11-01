9bdde7f147254c7183229978e65a9d6f86746d0c
Sharmaigne Angelie Mabano <sharmaignemabano@gmail.com>
2025-11-01 22:17:40 +0800

    test: add parse table passing test

---
+++ tests/test_parse_table.py
@@ -0,0 +1,25 @@
+import io
+
+from parsers.parse_table import create_parse_table
+
+
+def test_create_parse_table():
+    csv_content = """,id,+,*,(,),$
+                    E,1,,,1,,
+                    E’,,2,,,3,3
+                    T,4,,,4,,
+                    T’,,6,5,,6,6
+                    F,8,,,7,,"""
+
+    ptbl = io.StringIO(csv_content)
+
+    expected_parse_table = {
+        "E": {"id": 1, "(": 1},
+        "E’": {"+": 2, ")": 3, "$": 3},
+        "T": {"id": 4, "(": 4},
+        "T’": {"+": 6, "*": 5, ")": 6, "$": 6},
+        "F": {"id": 8, "(": 7},
+    }
+
+    result = create_parse_table(ptbl)
+    assert result == expected_parse_table
