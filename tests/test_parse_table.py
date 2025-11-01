fd35f8ae0229e26e5fdf90adf1481a4926111c8b
Sharmaigne Angelie Mabano <sharmaignemabano@gmail.com>
2025-11-01 23:04:43 +0800

    test: add tests for new dataclass

--- tests/test_parse_table.py
+++ tests/test_parse_table.py
@@ -1,17 +1,20 @@
 import io
 import textwrap
-from parsers.parse_table import create_parse_table
 
+from parsers.parse_table import ParseTable, get_terminals, load_parse_table
 
-def test_create_parse_table():
-    csv_content = textwrap.dedent("""\
+csv_content = textwrap.dedent(
+    """\
         ,id,+,*,(,),$
         E,1,,,1,,
         E’,,2,,,3,3
         T,4,,,4,,
         T’,,6,5,,6,6
-        F,8,,,7,,""")
+        F,8,,,7,,"""
+)
 
+
+def test_parse_table():
     ptbl = io.StringIO(csv_content)
 
     expected_parse_table = {
@@ -24,3 +27,26 @@ def test_create_parse_table():
 
     result = load_parse_table(ptbl)
     assert result == expected_parse_table
+
+
+def test_get_terminals():
+    ptbl = io.StringIO(csv_content)
+
+    expected_terminals = ["id", "+", "*", "(", ")", "$"]
+
+    result = get_terminals(ptbl)
+    assert result == expected_terminals
+
+
+class TestParseTableClass:
+    def test_parse_table_class_initialization(self):
+        ptbl = io.StringIO(csv_content)
+
+        expected_parse_table = load_parse_table(ptbl)
+
+        expected_terminals = get_terminals(ptbl)
+
+        parse_table_instance = ParseTable(ptbl, prod_rules=[])
+        assert parse_table_instance.table == expected_parse_table
+        assert parse_table_instance.terminals == expected_terminals
+        assert parse_table_instance.rules == []
